de937814b3d6f498d883fe5e875e9760769716d2
Kaif Khan <Kaifkhan9619@gmail.com>
2022-03-14 13:58:46 +0530

    back to start

--- src/App.js
+++ src/App.js
@@ -3,24 +3,17 @@ import './App.css';
 import Landing from "./Component/Landing";
 import Lesson from './Component/Lesson';
 import Navbar from './Component/Navbar';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+// import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 // import Sidebar from './Component/Navbar';
 
 function App() {
 
   return (
     <div className="App">
-
+        <Landing/>
         <div className='main-content'>
                 <Navbar/>
-          <Router>
-            <div>
-              <Routes>
-                <Router path='/' element={<Landing/>}/>
-                <Route path='courses' element={<Lesson/>}/>
-              </Routes>
-            </div>
-          </Router>
+                <Lesson/>
         </div>
     </div>
   );
