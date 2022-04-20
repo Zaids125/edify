87ee5e40865a4abeb27ec26b1cc3e8500e0089cf
zk-1460 <khanzaid1015@gmail.com>
2022-04-21 05:11:47 +0530

    Minor fixes

--- src/pages/MyCourses/MyCourses.js
+++ src/pages/MyCourses/MyCourses.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import MyCourseCard from "../../Component/MyCourseCard/MyCourseCard";
 import SideBar from "../../Component/SideBar/SideBar";
 import classes from "./MyCourses.module.css";
 
 function MyCourses() {
+  const [active, setActive] = useState(false);
   return (
     <div className={classes.MyCourses}>
       <div
@@ -16,9 +17,19 @@ function MyCourses() {
       </div>
       <div className={classes.MyCoursesContainer}>
         <div className={classes.CourseType}>
-          <p className={classes.CourseTypeActive}>Active</p>
+          <p
+            onClick={() => setActive((state) => !state)}
+            className={active ? classes.CourseTypeActive : ""}
+          >
+            Active
+          </p>
           <div></div>
-          <p>Completed</p>
+          <p
+            onClick={() => setActive((state) => !state)}
+            className={!active ? classes.CourseTypeActive : ""}
+          >
+            Completed
+          </p>
         </div>
         <div className={classes.line}></div>
         <div className={classes.AllCourses}>
